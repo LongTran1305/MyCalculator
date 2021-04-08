b988791d26ec2acd5dad2753fc6cd53a2689c66e
longtran <huylong.vipandrip@gmail.com>
2021-04-09 00:44:21 +0700

    first commit

---
+++ app/src/main/java/com/longtran/mycalculator/MainActivity.kt
@@ -0,0 +1,118 @@
+package com.longtran.mycalculator
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.view.View
+import android.widget.Button
+import android.widget.Toast
+import kotlinx.android.synthetic.main.activity_main.*
+
+class MainActivity : AppCompatActivity() {
+
+    var lastNumeric : Boolean = false
+    var lastDot : Boolean =  false
+    var lastClear : Boolean = false
+
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+    fun onDigit(view: View){
+        tvInput.append((view as Button).text)
+        lastNumeric = true
+        lastClear = true
+    }
+    fun onAllClear(view: View){
+        tvInput.text = ""
+        lastNumeric = false
+        lastDot = false
+    }
+    fun onClear (view: View){
+        var tvValue = tvInput.text.toString()
+        if(lastClear){
+            tvValue = tvValue.substring(0,tvValue.length -1)
+            tvInput.text = tvValue
+        }
+    }
+    fun onDecimalPoint(view: View){
+        if(lastNumeric && !lastDot){
+            tvInput.append(".")
+            lastNumeric = false
+            lastDot = true
+        }
+    }
+    fun onOperator(view: View){
+        if(lastNumeric && !isOperatorAdded(tvInput.text.toString())){
+            tvInput.append((view as Button).text)
+            lastNumeric = false
+            lastDot = false
+        }
+    }
+    private fun isOperatorAdded(value: String) : Boolean{
+        return if (value.startsWith("-")){
+            false
+        }else{
+            value.contains("/") || value.contains("*") || value.contains("+") || value.contains("-")
+        }
+    }
+    fun onEqual(view: View){
+        if(lastNumeric){
+            var tvValue = tvInput.text.toString()
+            var prefix = ""
+            try {
+                if(tvValue.startsWith("-")){
+                    prefix = "-"
+                    tvValue = tvValue.substring(1)
+                }
+                if(tvValue.contains("-")){
+                    val splitValue = tvValue.split("-")
+                    var one = splitValue[0]
+                    var two = splitValue[1]
+                    if(!prefix.isEmpty()){
+                        one = prefix + one
+                    }
+                    tvInput.text = removeZeroAfrerDot((one.toDouble() - two.toDouble()).toString())
+
+                }else if(tvValue.contains("+")){
+                    val splitValue = tvValue.split("+")
+                    var one = splitValue[0]
+                    var two = splitValue[1]
+                    if(!prefix.isEmpty()){
+                        one = prefix + one
+                    }
+                    tvInput.text =  removeZeroAfrerDot((one.toDouble() + two.toDouble()).toString())
+
+                }else if(tvValue.contains("/")){
+                    val splitValue = tvValue.split("/")
+                    var one = splitValue[0]
+                    var two = splitValue[1]
+                    if(!prefix.isEmpty()){
+                        one = prefix + one
+                    }
+                    tvInput.text =  removeZeroAfrerDot((one.toDouble() / two.toDouble()).toString())
+
+                }else if(tvValue.contains("*")){
+                    val splitValue = tvValue.split("*")
+                    var one = splitValue[0]
+                    var two = splitValue[1]
+                    if(!prefix.isEmpty()){
+                        one = prefix + one
+                    }
+                    tvInput.text =  removeZeroAfrerDot((one.toDouble() * two.toDouble()).toString())
+
+                }
+            }catch (e: ArithmeticException){
+                e.printStackTrace()
+            }
+        }
+        lastClear = false
+    }
+    private fun removeZeroAfrerDot(result: String ): String{
+        var value = result
+        if(result.contains(".0")){
+            value = result.substring(0,result.length-2)
+        }
+        return value
+    }
+}
